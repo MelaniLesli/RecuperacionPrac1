464fb13178bb8878848bcd1b3d9e78b52f4d8fc8
Melani Ramos <melaniramoslopes@melani.ramos@educa.madrid.org>
2022-01-31 02:29:44 +0100

    Continuo con modulo add

--- proyecto_contactos/__main__.py
+++ proyecto_contactos/__main__.py
@@ -1,5 +1,5 @@
-from actions.add import creo_contacto
 from help import show_help
+from inputs.input_add import input_add
 import sys
 import os
 
@@ -7,10 +7,4 @@ if __name__ == '__main__':
     if  sys.argv[1] == 'help':
        show_help()
     elif sys.argv[1] == 'add':
-       print(f'##Add new Contacts: {sys.argv[2]}')
-       telf = int(input('Tlf: '))
-       correo = str(input('Email: '))
-       ciudad = str(input('City: '))
-       local = str(input('location: '))
-       nac = str(input('Date of birth:'))
-       creo_contacto(sys.argv[2], telf, correo, ciudad, local, nac)
+       input_add()
---
+++ proyecto_contactos/inputs/input_add.py
@@ -0,0 +1,13 @@
+from actions.add import creo_contacto
+import sys
+import os
+
+def input_add():
+    print(f'##Add new Contacts: {sys.argv[2]}')
+    telf = int(input('Tlf: '))
+    correo = str(input('Email: '))
+    ciudad = str(input('City: '))
+    local = str(input('location: '))
+    nac = str(input('Date of birth:'))
+    creo_contacto(sys.argv[2], telf, correo, ciudad, local, nac)
+
